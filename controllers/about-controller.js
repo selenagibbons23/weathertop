797f5f7c551009c594ed6de85d8683ce7377f616
Glitch (weak-fern-neighborhood) <none>
2023-08-26 18:20:45 +0000

    🏗📙 Checkpoint ./controllers/about-controller.js:60638989/45

--- controllers/about-controller.js
+++ controllers/about-controller.js
@@ -1,7 +1,7 @@
 export const aboutController = {
   index(request, response) {
     const viewData = {
-      title: "About Playlist",
+      title: "About Station",
     };
     console.log("about rendering");
     response.render("about-view", viewData);
